6e2c3feaed04b33b4d400fdd7721bb4a0961c8d9
hieuphan2708 <pnhieu.ms@gmail.com>
2020-09-07 11:53:26 +0700

    a

--- assets/effect.js
+++ assets/effect.js
@@ -158,8 +158,8 @@ $('document').ready(function(){
 		$('#b66').animate({top:240, left: vw+150},500);
 		$('#b77').animate({top:240, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
-		$('.balloons h2').fadeIn(6000);
-		$(this).fadeOut(9000).delay(3000).promise().done(function(){
+		$('.balloons h2').fadeIn(4000);
+		$(this).fadeOut('slow').delay(3000).promise().done(function(){
 			$('#story').fadeIn('slow');
 		});
 	});
